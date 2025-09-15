f34fc4997aca4e6b903c2e0354a87a46c8d3b894
Helder Balbino <helderbalbino@googlemail.com>
2025-09-15 23:50:44 +0100

    Add TypeScript interfaces for quiz functionality

---
+++ src/types/quiz.ts
@@ -0,0 +1,22 @@
+export interface QuizQuestion {
+  id: number;
+  question: string;
+  options: string[];
+  correctAnswer: number;
+  explanation?: string;
+}
+
+export interface QuizState {
+  currentQuestionIndex: number;
+  answers: number[];
+  score: number;
+  isComplete: boolean;
+  showResults: boolean;
+}
+
+export interface QuizResult {
+  score: number;
+  totalQuestions: number;
+  percentage: number;
+  message: string;
+}
